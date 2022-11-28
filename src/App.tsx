deb3f17a444c913232aff907effd8563cbe04ad1
flpdorea <felipeldorea@gmail.com>
2022-11-27 21:34:46 -0300

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-function App() {
+import { Post } from "./Post";
+
+export function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <h1>Hello World!</h1>
+      <h1>Hello World!</h1>
+      <Post/>
+      <Post/>
+      <Post/>
+    </>
   )
 }
-
-export default App
