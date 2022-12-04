76f7ff7b32f390cb1829927998f7f7223ff19063
flpdorea <felipeldorea@gmail.com>
2022-12-04 20:57:23 -0300

    adding header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+
+import './global.css'
 
 export function App() {
   return (
     <>
+      <Header />
       <Post author='Felipe' content='Testando'/>
       <Post author='John' content='Testing'/>
     </>
