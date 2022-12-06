c049e78ccfe081057ab39af8d1eb85594ed7efbf
flpdorea <felipeldorea@gmail.com>
2022-12-06 11:44:19 -0300

    adding sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { Sidebar } from './components/Sidebar'
 import { Header } from './components/Header'
 import { Post } from './Post'
 
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
     <>
       <Header />
-      <Post author='Felipe' content='Testando'/>
-      <Post author='John' content='Testing'/>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author='Felipe' content='Testando'/>
+          <Post author='John' content='Testing'/>
+        </main>
+      </div>
     </>
   )
 }
